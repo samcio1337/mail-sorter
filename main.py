8f30815a205a967843e4b6b1a7a8bd707c5a8b8c
samcio1337 <you@example.com>
2024-03-05 21:09:36 +0100

    - Wyniki od teraz zapisują się w folderze "results" - Program automatycznie usuwa capture

--- main.py
+++ main.py
@@ -1,52 +1,45 @@
+import os
 import tkinter as tk
 from tkinter import filedialog
 
 def sort_emails(emails):
-    onet_emails = []
-    interia_emails = []
-    wp_emails = []
-    o2_emails = []
+    email_dict = {
+        "onet": [],
+        "interia": [],
+        "wp": [],
+        "o2": []
+    }
 
     for line in emails:
         if ":" in line:
             email, password = line.strip().split(':', 1)
             email_lower = email.lower()
             if email_lower.endswith(("@op.pl", "@onet.pl", "@poczta.onet.pl", "@vp.pl", "@onet.eu", "@autograf.pl", "@buziaczek.pl", "@spoko.pl", "@poczta.onet.eu", "@onet.com.pl", "@amorki.pl", "@opoczta.pl")):
-                onet_emails.append(line.strip())
+                email_dict["onet"].append(line.strip().split(' ', 1)[0])
             elif email_lower.endswith(("@interia.pl", "@poczta.fm", "@interia.eu", "@interia.com", "@intmail.pl", "@adresik.net", "@interiowy.pl", "@pisz.to", "@vip.interia.pl", "@pacz.to", "@ogarnij.se")):
-                interia_emails.append(line.strip())
+                email_dict["interia"].append(line.strip().split(' ', 1)[0])
             elif email_lower.endswith("@wp.pl"):
-                wp_emails.append(line.strip())
+                email_dict["wp"].append(line.strip().split(' ', 1)[0])
             elif email_lower.endswith("@o2.pl"):
-                o2_emails.append(line.strip())
+                email_dict["o2"].append(line.strip().split(' ', 1)[0])
 
-    if onet_emails:
-        output_filename = f"onet_{len(onet_emails)}.txt"
-        with open(output_filename, 'w') as output_file:
-            output_file.write('\n'.join(onet_emails))
+    if not os.path.exists("results"):
+        os.makedirs("results")
 
-    if interia_emails:
-        output_filename = f"interia_{len(interia_emails)}.txt"
-        with open(output_filename, 'w') as output_file:
-            output_file.write('\n'.join(interia_emails))
+    for email_type, email_list in email_dict.items():
+        if email_list:
+            output_filename = f"results/{email_type}_{len(email_list)}.txt"
+            emails_to_write = '\n'.join(email_list)
+            with open(output_filename, 'w') as output_file:
+                output_file.write(emails_to_write)
 
-    if wp_emails:
-        output_filename = f"wp_{len(wp_emails)}.txt"
-        with open(output_filename, 'w') as output_file:
-            output_file.write('\n'.join(wp_emails))
-    
-    if o2_emails:
-        output_filename = f"o2_{len(o2_emails)}.txt"
-        with open(output_filename, 'w') as output_file:
-            output_file.write('\n'.join(o2_emails))
-
-    root.destroy() 
+    root.destroy()
 
 def get_file():
     root.withdraw()
-    top = tk.Toplevel() 
+    top = tk.Toplevel()
     top.withdraw()
-    filename = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=[("Text files", "*.txt")])
+    filename = filedialog.askopenfilename(initialdir="/", title="Wybor pliku", filetypes=[("Text files", "*.txt")])
     if filename:
         with open(filename, 'r') as file:
             emails = file.readlines()
